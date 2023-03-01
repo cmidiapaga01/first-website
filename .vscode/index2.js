0cbc2b9e6aa5e64307538077f57ed397d3c00385
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2023-03-01 19:41:53 +0100

    testing

---
+++ .vscode/index2.js
@@ -0,0 +1,20 @@
+
+function square(x) {
+    var ans = x*x;
+    return ans;
+}
+var y = square(4);
+
+
+
+
+var x = 2;
+var y = 3;
+var z = x*z;
+print(z);
+{ var x = 2;
+var y = 3;
+var soma = 'x'*'y';
+const resultado = 
+console.log(soma)
+}
