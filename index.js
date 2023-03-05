76bd28ce553ff2962119624f2eaeb03a8cb18ae8
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2023-03-05 14:33:17 +0100

    comite

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
-function composicao(fn1, fn2, fn3, valor) {
-    return fn3(fn2(fn1(valor)))
-}
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
-function gritar (texto) {
-    return texto.toUpperCase()
-}
+const app = express();
 
-function enfatizar (texto) {
-    return '${texto}!!!!!'
-}
+const port = 3000;
 
-function solicitar (texto) {
-    return texto.split('').join(' ')
-}
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const resultado = composicao(
-    gritar
-    enfatizar
-    solicitar
-)('ARRAYYYYYYYYYYYYYYYYY')
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-console.log(resultado)
+app.get('/processar-formulario', function(req, res) {
+  const nome = req.body.nome;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'SEU_EMAIL_AQUI',
+      pass: 'SUA_SENHA_AQUI',
+    },
+  });
+
+  const mailOptions = {
+    from: 'SEU_EMAIL_AQUI',
+    to: 'tbusprp@gmail.com',
+    subject: 'Dados do Formulário de Contato',
+    text: `Nome: ${nome}`,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send('Ocorreu um erro ao enviar o formulário');
+    } else {
+      console.log('E-mail enviado: ' + info.response);
+      res.send('Formulário enviado com sucesso!');
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
